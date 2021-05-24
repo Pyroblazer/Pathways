026fe7decd38efb3aaac6ad1a3cd4d52d8452c16
Stefanny Roseliana <stefannyrslna@gmail.com>
2021-05-24 19:55:16 +0700

    Layout Section 3

---
+++ app/src/main/java/app/stefanny/pathway/WelcomePage.kt
@@ -0,0 +1,11 @@
+package app.stefanny.pathway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+    }
+}
---
+++ app/src/main/java/app/stefanny/pathway/preliminary/PreliminaryTechnicalTest.kt
@@ -0,0 +1,12 @@
+package app.stefanny.pathway.preliminary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.stefanny.pathway.R
+
+class PreliminaryTechnicalTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preliminary_technical_test)
+    }
+}
